c37a6c7dc74e912b92d2bcf2800c0ee4c4753223
Sean Rivera <vivereserive@gmail.com>
2026-05-26 13:16:11 +1200

    Updated get_json route to return jsonified messages

--- app.py
+++ app.py
@@ -12,11 +12,19 @@ def dashboard():
 
 @app.route("/get_json/data", methods=['POST'])
 def get_json():
+    client_key = request.headers.get("AQM-API-KEY") # Ensure that the API key is the same
+
+    # Check if client is the air quality monitor
+    if not client_key or client_key != AQM_API_KEY:
+        print("Imposter! Not my Air Quality Monitor!")
+        return jsonify({"status": "error", "message": "Unauthorized"}), 401 # Unauthorize Response
+    
+    # Check if package is a json file
     if request.is_json:
         data = request.get_json() # Get JSON file from ESP32
-        return "Success", 200 # Success Response
+        return jsonify({"status": "success", "message": "data received"}), 200 # Success Response
     else:
-        return "Error", 400 # Error Response
+        return jsonify({"status": "error", "message": "Not JSON Package"}), 400 # Error Response
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
